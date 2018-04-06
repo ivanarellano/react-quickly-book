ddee9d2a21765917d758ee496ecf897fbd73bea8
Ivan Arellano <ivan@diagnosticdriving.com>
2018-04-06 12:47:38 -0400

    Finish ch01

---
+++ ch01/hello-world/js/main.js
@@ -0,0 +1,12 @@
+var h1 = React.createElement('h1', null, 'Hello world!')
+
+class HelloWorld extends React.Component {
+    render() {
+        return React.createElement('div', null, h1, h1)
+    }
+}
+
+ReactDOM.render(
+    React.createElement(HelloWorld),
+    document.getElementById('content')
+)
