356d435be7649f952bfcceada2835b02585adf26
Ivan Arellano <ivan@diagnosticdriving.com>
2018-04-10 12:38:02 -0400

    Finish ch03

--- ch03/js/script.js
+++ ch03/js/script.js
@@ -1,4 +1,15 @@
-const container = document.getElementById('content');
+class DateTime extends React.Component {
+  render() {
+    let now = new Date().toLocaleString();
+    return React.createElement(
+      'span',
+      null,
+      'Current date and time is ',
+      now,
+      '.'
+    );
+  }
+}
 
 class HelloWorld extends React.Component {
   render() {
@@ -19,4 +30,69 @@ class HelloWorld extends React.Component {
   }
 }
 
-ReactDOM.render(React.createElement(HelloWorld, null), container);
+class HelloFramework extends React.Component {
+  render() {
+    return React.createElement(
+      'h1',
+      this.props,
+      'Hello ',
+      this.props.frameworkName,
+      '!'
+    );
+  }
+}
+
+class ProfileLink extends React.Component {
+  render() {
+    return React.createElement(
+      'a',
+      { href: this.props.url,
+        title: this.props.label,
+        target: '_blank' },
+      'Profile'
+    );
+  }
+}
+
+const ember = React.createElement(HelloFramework, {
+  id: 'ember',
+  frameworkName: 'Ember.js',
+  title: 'A framework for creating ambitious web applications.' });
+
+const backbone = React.createElement(HelloFramework, {
+  id: 'backbone',
+  frameworkName: 'Backbone.js',
+  title: 'Backbone.js gives structure to web applications...' });
+
+const angular = React.createElement(HelloFramework, {
+  id: 'angular',
+  frameworkName: 'Angular.js',
+  title: 'Superheroic JavaScript MVW Framework' });
+
+const container = document.getElementById('content');
+
+// ReactDOM.render(
+//   <HelloWorld/>,
+//   container
+// )
+
+ReactDOM.render(React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'a',
+    { href: 'http://ivanarellano.com' },
+    'Visit my website'
+  ),
+  React.createElement('br', null),
+  React.createElement(DateTime, { userName: 'Ivan' })
+), container);
+
+// ReactDOM.render(
+//   <div>
+//     {ember}
+//     {backbone}
+//     {angular}
+//   </div>,
+//   container
+// )
--- ch03/js/script.jsx
+++ ch03/js/script.jsx
@@ -1,7 +1,12 @@
-const container = document.getElementById('content');
+class DateTime extends React.Component {
+  render() {
+    let now = new Date().toLocaleString()
+    return <span>Current date and time is {now}.</span>
+  }
+}
 
 class HelloWorld extends React.Component {
-  render () {
+  render() {
     return (
       <div>
         <h1>1. Hello</h1>
@@ -11,7 +16,57 @@ class HelloWorld extends React.Component {
   }
 }
 
+class HelloFramework extends React.Component {
+  render() {
+    return <h1 {...this.props}>Hello {this.props.frameworkName}!</h1>
+  }
+}
+
+class ProfileLink extends React.Component {
+  render() {
+    return (
+      <a href={this.props.url}
+         title={this.props.label}
+         target="_blank">Profile</a>
+    )
+  }
+}
+
+const ember = <HelloFramework
+id='ember'
+frameworkName='Ember.js'
+title='A framework for creating ambitious web applications.'/>
+
+const backbone = <HelloFramework
+id='backbone'
+frameworkName='Backbone.js'
+title='Backbone.js gives structure to web applications...'/>
+
+const angular = <HelloFramework
+id='angular'
+frameworkName='Angular.js'
+title='Superheroic JavaScript MVW Framework'/>
+
+const container = document.getElementById('content');
+
+// ReactDOM.render(
+//   <HelloWorld/>,
+//   container
+// )
+
 ReactDOM.render(
-  <HelloWorld/>,
+  <div>
+    <a href="http://ivanarellano.com">Visit my website</a><br/>
+    <DateTime userName='Ivan'/>
+  </div>,
   container
 )
+
+// ReactDOM.render(
+//   <div>
+//     {ember}
+//     {backbone}
+//     {angular}
+//   </div>,
+//   container
+// )
